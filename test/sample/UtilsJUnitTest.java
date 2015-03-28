e4c5dd3a5aa808970c3dd558fb37a8aa2b88f376
masaki <marlcola2@gmail.com>
2015-03-29 01:12:05 +0900

    [modify] 予想された例外のテストメソッドを追加
    computeFactorial()メソッドの引数に負の数を指定することで、違法引数例外が発生するかテストします。

--- test/sample/UtilsJUnitTest.java
+++ test/sample/UtilsJUnitTest.java
@@ -43,35 +43,51 @@ public class UtilsJUnitTest extends TestCase {
 
     // testComputeFactorial()を削除
     // testWithTimeout()を追加
-    public void testWithTimeout () throws InterruptedException, TimeoutException {
+    public void testWithTimeout() throws InterruptedException, TimeoutException {
         // 実行確認用の出力
         System.out.println("* UtilsJUnit3Test: test method 2 - testWithTimeout()");
         // 1～30001までのランダムな整数
         final int factorialOf = 1 + (int) (30000 * Math.random());
         // 生成したランダム整数を出力
         System.out.println("computing: " + factorialOf + "!");
-        
+
         // テストスレッド
         Thread testThread = new Thread() {
             @Override
-            public void run () {
+            public void run() {
                 // ランダム整数の階乗を出力
                 System.out.println(factorialOf + "! = " + Utils.computeFactorial(factorialOf));
             }
         };
-        
+
         // テストスレッドをスタート
         testThread.start();
         // スレッドを1000ミリ秒でスリープ
         Thread.sleep(1000);
         // テストスレッドを割り込み
         testThread.interrupt();
-        
+
         // テストスレッドが割り込みしたら
         if (testThread.isInterrupted()) {
             // タイムアウト例外をスロー
             throw new TimeoutException("the test took too long to complete");
         }
+    }
+
+    public void testExpectedException() {
+        // 実行確認用の出力
+        System.out.println("* UtilsJUnit3Test: test method 3 - testExpectedException()");
+        try {
+            // わざと例外をスローさせるために、負の数を定義
+            final int factorialOf = -5;
+            // 違法引数例外が発生するはず
+            System.out.println(factorialOf + "! = " + Utils.computeFactorial(factorialOf));
+            // 失敗したらメッセージを出力
+            fail("IllegalArgumentException was expected");
+        } catch (IllegalArgumentException ex){
+            // 違法引数例外が発生したら実行(今回は空)
+        }
+
     }
 
     /**
