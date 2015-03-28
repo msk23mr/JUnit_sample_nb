eee78f88f62d5f9d6d746cbddf2116c4709a1b79
masaki <marlcola2@gmail.com>
2015-03-28 23:53:40 +0900

    [modify] scalarMaultiplication()メソッドのテストメソッドを変更

--- test/sample/VectorsJUnit3Test.java
+++ test/sample/VectorsJUnit3Test.java
@@ -12,7 +12,7 @@ import junit.framework.TestCase;
  * @author mori
  */
 public class VectorsJUnit3Test extends TestCase {
-    
+
     public VectorsJUnit3Test(String testName) {
         super(testName);
     }
@@ -35,14 +35,15 @@ public class VectorsJUnit3Test extends TestCase {
      * Test of scalarMultiplication method, of class Vectors.
      */
     public void testScalarMultiplication() {
-        System.out.println("scalarMultiplication");
-        int[] a = null;
-        int[] b = null;
-        int expResult = 0;
-        int result = Vectors.scalarMultiplication(a, b);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        // テスト結果を識別しやすいように変更
+        System.out.println("* VectorsJUnit3Test: testScalarMultiplication()");
+        // テストスケルトンを除去
+        // アサーションを追加
+        assertEquals(0, Vectors.scalarMultiplication(new int[]{0, 0}, new int[]{0, 0}));
+        assertEquals(39, Vectors.scalarMultiplication(new int[]{3, 4}, new int[]{5, 6}));
+        assertEquals(-39, Vectors.scalarMultiplication(new int[]{-3, 4}, new int[]{5, -6}));
+        assertEquals(0, Vectors.scalarMultiplication(new int[]{5, 9}, new int[]{-9, 5}));
+        assertEquals(100, Vectors.scalarMultiplication(new int[]{6, 8}, new int[]{6, 8}));
     }
-    
+
 }
